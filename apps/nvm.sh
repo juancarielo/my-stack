c52126b9b82982a4e896e8bdc46c45a8251bfa0d
Juan Carielo <juancarielo@gmail.com>
2017-10-05 18:56:49 -0300

    Fix nvm

--- apps/nvm.sh
+++ apps/nvm.sh
@@ -3,5 +3,6 @@
 sudo apt-get update;
 sudo apt-get install -y build-essential libssl-dev;
 curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh; # Check new version in https://github.com/creationix/nvm
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh";  # This loads nvm
 nvm install 6.0.0;
 nvm use 6.0.0;
