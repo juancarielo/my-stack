cea0df1d1ba718813ec2e36745e8f09ea2e9ed8f
Juan Carielo <juancarielo@gmail.com>
2017-12-17 19:55:42 -0200

    Updated sublime-text app

--- apps/sublime-text.sh
+++ apps/sublime-text.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:webupd8team/sublime-text-3;
-sudo apt-get update;
-sudo apt-get install -y sublime-text-installer;
+# sudo add-apt-repository ppa:webupd8team/sublime-text-3;
+# sudo apt-get update;
+# sudo apt-get install -y sublime-text-installer;
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -;
+sudo apt-get install apt-transport-https;
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list;
