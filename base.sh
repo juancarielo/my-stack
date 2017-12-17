2df302cb72cf12b7d2d226dd126ded137f189aa8
Juan <juan@dell.localdomain>
2017-12-17 19:25:33 -0200

    Fix enter key when to select an app

--- base.sh
+++ base.sh
@@ -5,7 +5,7 @@ confirm() {
     # read -r -p "${1:-Do you wish to install this program? [y/n]}" response
     read -p "Do you wish to install $1? (y/n) " yn
     case "$yn" in
-        [yY][eE][sS]|[yY])
+        [yY][eE][sS]|[yY]|"")
             true
             ;;
         *)
